5007037b56c256587d929d2434b9cdbf486dea6a
Ana Isabel <imacswv@gmail.com>
2018-05-30 12:44:43 +0200

    Regitering the new models in the admin

--- blackcat/blackcat/storysharing/admin.py
+++ blackcat/blackcat/storysharing/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Story, Snippet
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Story)
+admin.site.register(Snippet)
