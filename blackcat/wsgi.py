9124eb1b6d8bab92115861aefcf73f8c2b6e35db
Ana Isabel <imacswv@gmail.com>
2018-06-19 13:47:29 +0200

    Including a openshift-friendly wsgi.py

--- blackcat/wsgi.py
+++ blackcat/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for blackcat project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blackcat.settings")
-
-application = get_wsgi_application()
+from blackcat.wsgi import application
